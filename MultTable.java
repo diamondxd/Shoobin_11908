9ac3d548c12636b5cf224fa3ead761f41634a7f0
diamondxd <55555428+diamondxd@users.noreply.github.com>
2019-10-08 19:27:00 +0300

    Таблица умножения )

---
+++ MultTable.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+ public class MultTable {
+	 
+	 public static void main(String [] args) {
+		 
+		 System.out.print("    Enter table size: ");
+		 
+		 Scanner ss = new Scanner(System.in);
+		 
+		 int x = ss.nextInt();
+		 
+		 System.out.printf("%4s","*");
+		 System.out.printf("%4s","|");
+		 
+		 
+		  for(int i = 1; i <= x; i++) {
+			 
+			  System.out.printf("%4d", i);
+			 
+		  }
+		  
+		  System.out.println();
+		 
+		  for(int i = 1; i <= 4*(x+3); i++) {
+			 
+			  System.out.print("-");
+			 
+		  }
+		  
+		  System.out.println();
+		  
+		  for(int i = 1; i <= x; i++) {
+			  
+			  System.out.printf("%4d", i);
+		      System.out.printf("%4s","|");
+			  
+			   for(int j = 1; j <= x; j++) {
+				  
+				  System.out.printf("%4d", i*j);
+				  
+				  
+				  
+			   }
+			  
+			  System.out.println();
+			  
+		  }
+		 
+		
+		 
+	 }
+ }
