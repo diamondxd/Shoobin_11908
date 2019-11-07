8878af2261febd272dfac98872dfed6fb6836053
Dmitry Shoobin <dima_orc@mail.ru>
2019-11-07 11:35:10 +0300

    Regex

---
+++ Regex.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = "C:\\Users\\Администратор\\Documents\\NetBeansProjects\\JavaLessonsProject\\image.jpg:";
+
+        String regex = "(\\w+)\\.(jpg|png|bmp)\\b";
+
+        Pattern pt = Pattern.compile(regex);
+
+        Matcher matcher = pt.matcher(s);
+
+        while (matcher.find()) {
+
+
+            System.out.println(matcher.group(1));
+
+            System.out.println(matcher.group(2));
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
